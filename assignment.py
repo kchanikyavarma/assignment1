be5d0451d1569d1d2e6fe5280b18b79b95559fa4
kchanikyavarma <133203537+kchanikyavarma@users.noreply.github.com>
2023-07-03 08:29:17 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,51 @@
+#num=int(input("enter a number: "))
+#sum=0
+#for value in range(1, number +1):
+ #   sum = sum + value
+#print(sum)    
+
+#word="Python"
+#for i in range(1,len(word)+1):
+ #   new=""
+  #  for j in range(i):
+   #     new=new+(word[j])
+
+    #print(new*i)    
+
+
+#my_str = 'I am a student who is currently pursuing MBA'
+# split() and rsplit() with parameters
+#print(my_str.split(None, 3))
+#print(my_str.rsplit(None, 3))
+
+
+#a= "raju is good boy"
+#b= a.split()
+ #stringname[stringlength::-1]
+#for item in b[::-1]:
+ #   print(item,end=" ")
+#print()    
+
+
+#word= "Python"
+#for i in range(1,len(word)+1):
+ #   for j in range(i):
+  #      print(word[j],end="")
+   # print()
+#for i in range(1,len(word)+1):
+ #   for j in range(len(word)-i):
+  #      print(word[j],end="")
+   # print() 
+
+#l=[10,20,30,20,50]
+#print(l.remove(20))
+ 
+
+#num=4
+#n=1
+#for i in range(1,num+1):
+   # res=""
+    #for j in range(1,i+1):
+     #   res=str(n)+res
+      #  n+=1
+    #print(res)    
